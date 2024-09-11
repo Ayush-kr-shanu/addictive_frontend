247efa03d55e6a5066ac5ca8b5b38076c565da7f
Ayush-kr-shanu <aksmilestone35@gmail.com>
2024-09-11 11:47:48 +0530

    home page dummy

--- src/API/axios.config.js
+++ src/API/axios.config.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const url = 'http://localhost:8080/api'
+// const url = 'http://localhost:8080/api'
+const url = 'http://43.205.38.16:808/api'
 
 const axiosInstance = axios.create({
   baseURL: url,
---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function Home(){
+    return (
+        <div>
+            <h1>Welcome to Home Page</h1>
+            <p>This is a simple React App with routing and authentication.</p>
+        </div>
+    );
+}
